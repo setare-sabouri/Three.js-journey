f16f99da232eed45c1905dc67c04d71ccc07cc7b
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-06-18 21:39:39 +0200

    group + axes + transforms

--- basics/exercise/src/script.js
+++ basics/exercise/src/script.js
@@ -1,30 +1,51 @@
 import * as THREE from 'three'
-console.log(THREE);
-
 const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
 
-const cubeGeo = new THREE.BoxGeometry(1, 1, 1); ///badan 1 ro bardar
-const cubeMat = new THREE.MeshBasicMaterial({ color: 'blue' });
-const cubeObj = new THREE.Mesh(cubeGeo, cubeMat);
-scene.add(cubeObj);
+//group
+const container = new THREE.Group()
+scene.add(container)
+container.position.set(0, 1, -1)
+// cubes
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+)
+container.add(cube1)
 
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+cube2.position.set(1.5, 0, 0)
+container.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: '#00ff00' })
+)
+cube3.position.set(-1.5, 0, 0)
+container.add(cube3)
+//camera
 const sizes = {
     width: 800,
     height: 600
 }
-
-const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height); //play with fov 
-camera.position.z = 3
-camera.position.x = 1
-camera.position.y = 0.5
-
+const camera = new THREE.PerspectiveCamera(65, sizes.width / sizes.height);
+camera.position.set(2, 2, 3)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
 scene.add(camera);
+
+
+
+
+//rendering 
 
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({
     canvas
 });
-
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
