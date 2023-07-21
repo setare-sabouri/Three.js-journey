cd0e96144383fe67046d9abe4c4cd780ec1f69b7
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-07-21 02:46:18 +0200

    buffer

--- particles/src/objects/paticles.js
+++ particles/src/objects/paticles.js
@@ -1,9 +1,22 @@
 import * as THREE from 'three'
 
+const count = 500 //particles number 
+
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z) ---array of count * 3
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    console.log((Math.random() - 0.5) * 10);
+}
+
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 
 export const particle = new THREE.Points(
-    new THREE.SphereGeometry(2, 50, 20),
+    particlesGeometry,
     new THREE.PointsMaterial({
+        color: 0xff0000,
         size: 0.05,
         sizeAttenuation: true
     })
