3c3edef13530eee40b7b89d57d4d562ad0487556
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-03 02:42:14 +0200

    cm

--- 2.house/src/objects/ghosts.js
+++ 2.house/src/objects/ghosts.js
@@ -6,17 +6,17 @@ import { lightsList } from '../lightsSys/lights'
 export const ghostsCircle = () => {
     const elapsedTime = clock.getElapsedTime()
 
-    lightsList.ghost1.position.x = Math.cos(elapsedTime * 0.9) * 6 //avali sorat , dovomi shoa
+    lightsList.ghost1.position.x = Math.cos(elapsedTime * 0.9) * 6 //first=speed , second= radius
     lightsList.ghost1.position.z = Math.sin(elapsedTime * 0.9) * 6
     lightsList.ghost1.position.y = Math.sin(elapsedTime * 4)
 
 
-    lightsList.ghost2.position.x = Math.cos(-elapsedTime * 1.2) * 5 //abi
+    lightsList.ghost2.position.x = Math.cos(-elapsedTime * 1.2) * 5 //blue
     lightsList.ghost2.position.z = Math.sin(-elapsedTime * 1.2) * 8 + Math.sin(elapsedTime) * 6
     lightsList.ghost2.position.y = Math.sin(-elapsedTime * 5) + 1.5
 
 
-    lightsList.ghost3.position.x = Math.cos(elapsedTime * 1.5) * 8 //zard
+    lightsList.ghost3.position.x = Math.cos(elapsedTime * 1.5) * 8 //yellow
     lightsList.ghost3.position.z = Math.sin(elapsedTime * 1.5) * 8
     lightsList.ghost3.position.y = Math.sin(elapsedTime * 2) + Math.sin(elapsedTime * 4) + 1.5
 
